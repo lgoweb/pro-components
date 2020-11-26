dd66f66ad4a843ca887a450b2e5b98229c1ed270
ChenYCL <aa34913@126.com>
2020-11-26 18:39:04 +0800

    chore: remove hotjar

--- .dumi/theme/layout.tsx
+++ .dumi/theme/layout.tsx
@@ -9,26 +9,7 @@ import './layout.less';
 moment.locale('zh-cn');
 
 export default ({ children, ...props }: IRouteComponentProps) => {
-  useEffect(() => {
-    (function (h, o, t, j, a, r) {
-      // @ts-ignore
-      h.hj =
-        // @ts-ignore
-        h.hj ||
-        function () {
-          // @ts-ignore
-          (h.hj.q = h.hj.q || []).push(arguments);
-        };
-      // @ts-ignore
-      h._hjSettings = { hjid: 2036108, hjsv: 6 };
-      a = o.getElementsByTagName('head')[0];
-      r = o.createElement('script');
-      r.async = 1;
-      // @ts-ignore
-      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-      a.appendChild(r);
-    })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
-  }, []);
+  useEffect(() => {}, []);
   return (
     <ConfigProvider locale={zhCN}>
       <Layout {...props}>{children}</Layout>
