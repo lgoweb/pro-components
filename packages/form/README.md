7bef7d5b413d4608de58914748a9cc89553aeec6
ChenYCL <aa34913@126.com>
2020-11-27 16:46:00 +0800

    test: 多包发布

---
+++ packages/form/README.md
@@ -0,0 +1,18 @@
+# @lekp/pro-form
+
+> @lekp/pro-form.
+
+
+## Install
+
+Using npm:
+
+```bash
+$ npm install --save @lekp/pro-form
+```
+
+or using yarn:
+
+```bash
+$ yarn add @lekp/pro-form
+```
---
+++ packages/form/demos/base.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+//import Form from '@lekp/pro-form';
+import Form from '../src/index';
+
+export default () => {
+  return (
+    <>
+      <Form size={300} />
+    </>
+  );
+};
+  
