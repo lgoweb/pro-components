e62a26f7fd4ecef666b2fe891a1a7c454ae38fdc
ChenYCL <aa34913@126.com>
2020-12-10 18:21:43 +0800

    feat: theme

---
+++ packages/theme/README.md
@@ -0,0 +1,17 @@
+# @lekp/pro-theme
+
+> @lekp/pro-theme.
+
+## Install
+
+Using npm:
+
+```bash
+$ npm install --save @lekp/pro-theme
+```
+
+or using yarn:
+
+```bash
+$ yarn add @lekp/pro-theme
+```
---
+++ packages/theme/demos/base.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+//import Theme from '@lekp/pro-theme';
+import Theme from '../src/index';
+
+export default () => {
+  return (
+    <>
+      <Theme size={300} />
+    </>
+  );
+};
